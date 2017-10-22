533cda50100b32c9d44aabee8250e0c0db42755a
Marcelo Daniel <marcelodaniel.daniel@gmail.com>
2017-10-21 22:13:32 -0300

    fix fatal error PDO
    faltando a barra "\".

--- bootstrap/ContainerServices.php
+++ bootstrap/ContainerServices.php
@@ -120,9 +120,9 @@ class ContainerServices
                 $data['password']
             );
 
-            $database->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            $database->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
-            $database->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+            $database->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+            $database->setAttribute(\PDO::ATTR_EMULATE_PREPARES, false);
+            $database->setAttribute(\PDO::ATTR_DEFAULT_FETCH_MODE, \PDO::FETCH_ASSOC);
             $database->exec("set names utf8");
 
             return $database;
